d6006003a9fc1d1c97ea2543d571c550c450555a
Jannek Schröter <74050536+arercon@users.noreply.github.com>
2024-12-19 16:35:11 +0100

    Class Block + Blockchain added

---
+++ blockchain.class.js
@@ -0,0 +1,26 @@
+class Blockchain {
+    constructor(){
+        this.chain = [];
+    }
+
+    addBlock(block){
+        block.lastHash = this.getLastBlock().createHash();
+        this.chain.push(Object.freeze(block))
+    }
+
+    isValid(){
+        let invalidBlock = this.chain.find( (currBlock, i) => {
+            let prevBlock = this.chain[i - 1];
+            return prevBlock && prevBlock.createHash() != currBlock.lastHash;
+        })
+        if(invalidBlock){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    getLastBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+}
